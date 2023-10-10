9775f2d55293a7a568f341b1c87793dfa2cd9c18
jvogt23 <jamesvogt70@gmail.com>
2023-10-10 18:49:35 -0400

    Fish and Aquarium classes created

---
+++ Aquarium.java
@@ -0,0 +1,6 @@
+public class Aquarium {
+    public static void main(String[] args) {
+        Fish fish1 = new Fish("Dory", 25.015, 16.015);
+        System.out.println(fish1);
+    }
+}
---
+++ Fish.java
@@ -0,0 +1,106 @@
+/**
+ * Represents a Fish object.
+ * @author jvogt33
+ */
+public class Fish {
+    //Data
+    protected String name;
+    protected Double length;
+    protected Double weight;
+    //Only modify in the body of Fish constructor
+    private static int totalFish = 0;
+
+    /**
+     * Constructor for Fish object
+     * @param name The name of the Fish
+     * @param length The length of the Fish, as a positive Double
+     * @param weight The weight of the Fish as a positive Double
+     */
+    public Fish(String name, Double length, Double weight) {
+        if (name == null || name.length() == 0 || name.trim() == "") {
+            this.name = "Nemo";
+        } else {
+            this.name = name;
+        }
+
+        if (Double.isNaN(length) || Double.isInfinite(length) || 
+            length <= 0) {
+                this.length = 8.0;
+        } else {
+            this.length = length;
+        }
+
+        if (Double.isNaN(weight) || Double.isInfinite(weight) || 
+            weight <= 0) {
+                this.weight = 2.0;
+        } else {
+            this.weight = weight;
+        }
+        totalFish++;
+    }
+
+    /**
+     * Constructor for a Fish with default values
+     */
+    public Fish() {
+        this("Nemo", 5.0, 12.0);
+    }
+
+    /**
+     * Copy constructor for Fish object from other Fish
+     */
+    public Fish(Fish other) {
+        this(other.name, other.length, other.weight);
+    }
+
+    //Methods
+    /**
+     * formatLength
+     * @return String interpretation of this Fish's length
+     */
+    public String formatLength() {
+        String o = "";
+        double[] splitUnits = separateUnits(length, 12);
+        int ft = (int) splitUnits[0];
+        double in = splitUnits[1];
+        o = String.format("%d ft %.2f in", ft, in);
+        return o;
+    }
+
+    /**
+     * formatWeight
+     * @return String interpretation of this Fish's weight
+     */
+    public String formatWeight() {
+        String o = "";
+        double[] splitUnits = separateUnits(weight, 16);
+        int lb = (int) splitUnits[0];
+        double oz = splitUnits[1];
+        if (lb > 1) {
+            o = String.format("%d lbs %.2f oz", lb, oz);
+        } else {
+            o = String.format("%d lb %.2f oz", lb, oz);
+        }
+        return o;
+    }
+
+    /**
+     * String interpretation of this Fish
+     */
+    @Override
+    public String toString() {
+        return String.format(
+            "I'm a talking fish named %s. My length is %s and my weight is %s.",
+            this.name, this.formatLength(), this.formatWeight());
+    }
+
+    private double[] separateUnits(Double in, int distBetweenUnits) {
+        double[] o = new double[2];
+        int big = (int) (in / distBetweenUnits);
+        double small = in - (big * distBetweenUnits);
+        o[0] = big;
+        o[1] = small;
+        return o;
+    }
+
+}
